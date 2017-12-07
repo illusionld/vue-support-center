a275791de01256e4bbe2c0863ff415fa77f4866c
illusionld <illusionld@gmail.com>
2017-12-07 13:41:30 -0500

    initial commit

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app-layout">
+        <header class="header">
+            <div>
+                <img src="../assets/logo.svg" class="img">
+            </div>
+            <div>Security First</div>
+        </header>
+
+        <!-- Menu will be here -->
+        <router-view />
+    </div>
+</template>
+
+<style lang="stylus">
+    @import '../style/main';
+</style>
+
+<style lang="stylus" scoped>
+    .header {
+        .img {
+            width: 64px;
+            height: 64px;
+        }
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import AppLayout from './components/AppLayout.vue'
+import router from './router'
+
+new Vue({
+    el: '#app',
+    render: h => h(AppLayout),
+    // Provide the router to the app
+    router
+})
